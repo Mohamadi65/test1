e0495f203bf2f63da02a3188a56a30adb5514f43
Mohamadi65 <freelancemohamadi65@gmail.com>
2023-02-01 05:35:06 +0100

    Ajout de la fonction Unique

--- Mouna/prototype_V1.py
+++ Mouna/prototype_V1.py
@@ -1,16 +1,16 @@
 import pandas as pd
 
 
-def Not_Null(nom_colonne):
+def Not_Null(fichier,nom_colonne):
 
-        lire = pd.read_csv("C:/code/python/Mouna/Données-Etudiants.csv", header=0, sep=";")
 
-        for i in range(len(lire)):
+
+        for i in range(len(fichier)):
             # print(format(row.id,'0.0f'), row.nom)
-            essayer = lire.loc[i, nom_colonne]
+            valeur = fichier.loc[i, nom_colonne]
 
-            if str(essayer) == 'nan' or str(essayer)=='null':
-                #print("différent de null", format(essayer, '0.0f'))
+            if str(valeur) == 'nan' or str(valeur)=='null':
+                #print("différent de null", format(valeur, '0.0f'))
 
 
 
@@ -26,5 +26,37 @@ def Not_Null(nom_colonne):
 
 
 
+def Unique(fichier,nom_colonne):
+    # déclaration d'une liste pour enrégistrer les données uniques
+    liste=[]  # au début la liste est vide
+    for i in range(len(fichier)):
+        valeur = fichier.loc[i, nom_colonne]
+        liste.append(valeur)
+
+    for i in range(len(fichier)):
+        valeur = fichier.loc[i, nom_colonne]
+        if (str(valeur) != 'nan' or str(valeur)!='null') and liste.count(valeur)>1:
+            if type(valeur)=='int':
+
+                valeur=format(valeur,'0.0f')
+
+
+            with open("fichier_log.txt", "a", encoding='utf-8') as f:
+                f.write("la valeur  : ")
+                f.write(str(valeur))
+                f.write(" qui est presente à la ligne ")
+                f.write(str(i+2))
+                f.write(" de la colonne ")
+                f.write("'")
+                f.write(nom_colonne)
+                f.write("'")
+                f.write(" n'est pas unique\n")
+
+
+
+
+
 if __name__ == '__main__':
-    Not_Null('id_dept')
+    fichier = pd.read_csv("C:/code/python/Mouna/Données-Etudiants.csv", header=0, sep=";")
+    #Not_Null(fichier,'id_dept')
+    Unique(fichier,'nom')
